3de4885fcf7d8834a8dd14db2e7416d4a6dedf9d
rcon <reg710@gmail.com>
2024-03-19 14:23:03 -0400

    Remove bootstrap due to incompatible dependency for upgrading to angular 15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LocationSearchComponent } from './location-search/location-search.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
 import { RecommendationComponent } from './recommendation/recommendation.component';
@@ -18,7 +17,6 @@ import { RecommendationComponent } from './recommendation/recommendation.compone
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
     FormsModule,
     HttpClientModule,
   ],
